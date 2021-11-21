eb37ae88e3e7dee8488845c4bd428363a5cff873
Cyberooo <julien.erhard@edu.univ-fcomte.fr>
2021-11-21 19:29:47 +0100

    ajout plateaux

---
+++ src/newton/controller/ControllerJouer.java
@@ -0,0 +1,4 @@
+package newton.controller;
+
+public class ControllerJouer {
+}
--- src/newton/model/De.java
+++ src/newton/model/De.java
@@ -2,5 +2,12 @@ package newton.model;
 
 import org.mockito.Mockito;
 
+import java.util.Random;
+
 public class De {
+    private Random random;
+
+    public De(Random random) {
+        this.random = random;
+    }
 }
