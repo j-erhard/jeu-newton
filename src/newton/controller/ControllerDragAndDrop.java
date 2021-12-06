7b55f5ab443590756556cc42df34c0afd0f76e0e
ImSlaym <72543891+ImSlaym@users.noreply.github.com>
2021-12-06 21:21:58 +0100

    Ajout d'une liste et d'un for pour les targets et une liste pour les sources

--- src/newton/controller/ControllerDragAndDrop.java
+++ src/newton/controller/ControllerDragAndDrop.java
@@ -13,6 +13,7 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Objects;
 
 
@@ -26,119 +27,104 @@ public class ControllerDragAndDrop extends Application {
 
 
         Image test = new Image("newton/ressources/image/carte-Pokemon-18-111-Leviator-GX-240-PV.png", 200,200,true, true);
-        final ImageView source = new ImageView();
-        source.setImage(test);
+
+        final ImageView source1 = new ImageView();
+        source1.setImage(test);
 
         Image test2 = new Image("newton/ressources/image/919cr4oj5QL.png", 200,200,true, true);
-        final ImageView target = new ImageView();
-        target.setImage(test2);
-        target.setX(500);
 
-        final ImageView target3 = new ImageView();
-        target3.setImage(test2);
-        target3.setX(800);
+        final ImageView source2 = new ImageView();
+        source2.setImage(test2);
+        source2.setX(500);
+
+        final ImageView target1 = new ImageView();
+        target1.setImage(test2);
+        target1.setX(800);
 
 
-        source.setOnDragDetected(new EventHandler <MouseEvent>() {
-            public void handle(MouseEvent event) {
-                /* drag was detected, start drag-and-drop gesture*/
-                System.out.println("onDragDetected");
+        ArrayList<ImageView> sources = new ArrayList<>();
+        sources.add(source1);
+        sources.add(source2);
 
-                /* allow any transfer mode */
-                Dragboard db = source.startDragAndDrop(TransferMode.ANY);
+        ArrayList<ImageView> targets = new ArrayList<>();
+        targets.add(target1);
 
-                /* put a string on dragboard */
-                ClipboardContent content = new ClipboardContent();
-                content.putImage(source.getImage());
-                db.setContent(content);
+        for( ImageView source:sources) {
+            source.setOnDragDetected(new EventHandler<MouseEvent>() {
+                public void handle(MouseEvent event) {
+                    /* drag was detected, start drag-and-drop gesture*/
+                    System.out.println("onDragDetected");
 
-                event.consume();
-            }
-        });
+                    /* allow any transfer mode */
+                    Dragboard db = source.startDragAndDrop(TransferMode.ANY);
 
-        target.setOnDragDetected(new EventHandler <MouseEvent>() {
-            public void handle(MouseEvent event) {
-                /* drag was detected, start drag-and-drop gesture*/
-                System.out.println("onDragDetected");
+                    /* put a string on dragboard */
+                    ClipboardContent content = new ClipboardContent();
+                    content.putImage(source.getImage());
+                    db.setContent(content);
 
-                /* allow any transfer mode */
-                Dragboard db = target.startDragAndDrop(TransferMode.ANY);
-
-                /* put a string on dragboard */
-                ClipboardContent content = new ClipboardContent();
-                content.putImage(target.getImage());
-                db.setContent(content);
-
-                event.consume();
-            }
-        });
-
-        target3.setOnDragOver(new EventHandler <DragEvent>() {
-            public void handle(DragEvent event) {
-                /* data is dragged over the target */
-                System.out.println("onDragOver");
-
-                /* accept it only if it is  not dragged from the same node
-                 * and if it has a string data */
-                if (event.getGestureSource() != target3 &&
-                        event.getDragboard().hasImage()) {
-                    /* allow for both copying and moving, whatever user chooses */
-                    event.acceptTransferModes(TransferMode.MOVE);
+                    event.consume();
                 }
+            });
 
-                event.consume();
-            }
-        });
+            source.setOnDragDone(new EventHandler<DragEvent>() {
+                public void handle(DragEvent event) {
+                    /* the drag-and-drop gesture ended */
+                    System.out.println("onDragDone");
+                    /* if the data was successfully moved, clear it */
+                    if (event.getTransferMode() == TransferMode.MOVE) {
+                        source.setVisible(false);
+                        System.out.println("prout");
+                    }
 
-        target3.setOnDragDropped(new EventHandler <DragEvent>() {
-            public void handle(DragEvent event) {
-                /* data dropped */
-                System.out.println("onDragDropped");
-                /* if there is a string data on dragboard, read it and use it */
-                Dragboard db = event.getDragboard();
-                boolean success = false;
-                if (db.hasImage()) {
-                    target3.setImage(db.getImage());
-                    success = true;
+                    event.consume();
                 }
-                /* let the source know whether the string was successfully
-                 * transferred and used */
-                event.setDropCompleted(success);
+            });
+            root.getChildren().add(source);
+        }
+        for (ImageView target: targets) {
 
-                event.consume();
-            }
-        });
+            target.setOnDragOver(new EventHandler <DragEvent>() {
+                public void handle(DragEvent event) {
+                    /* data is dragged over the target */
+                    System.out.println("onDragOver");
 
-        source.setOnDragDone(new EventHandler <DragEvent>() {
-            public void handle(DragEvent event) {
-                /* the drag-and-drop gesture ended */
-                System.out.println("onDragDone");
-                /* if the data was successfully moved, clear it */
-                if (event.getTransferMode() == TransferMode.MOVE) {
-                    source.setVisible(false);
-                    System.out.println("prout");
+                    /* accept it only if it is  not dragged from the same node
+                     * and if it has a string data */
+                    if (event.getGestureSource() != target &&
+                            event.getDragboard().hasImage()) {
+                        /* allow for both copying and moving, whatever user chooses */
+                        event.acceptTransferModes(TransferMode.MOVE);
+                    }
+
+                    event.consume();
                 }
+            });
 
-                event.consume();
-            }
-        });
+            target.setOnDragDropped(new EventHandler <DragEvent>() {
+                public void handle(DragEvent event) {
+                    /* data dropped */
+                    System.out.println("onDragDropped");
+                    /* if there is a string data on dragboard, read it and use it */
+                    Dragboard db = event.getDragboard();
+                    boolean success = false;
+                    if (db.hasImage()) {
+                        target.setImage(db.getImage());
+                        success = true;
+                    }
+                    /* let the source know whether the string was successfully
+                     * transferred and used */
+                    event.setDropCompleted(success);
 
-        target.setOnDragDone(new EventHandler <DragEvent>() {
-            public void handle(DragEvent event) {
-                /* the drag-and-drop gesture ended */
-                System.out.println("onDragDone");
-                /* if the data was successfully moved, clear it */
-                if (event.getTransferMode() == TransferMode.MOVE) {
-                    target.setVisible(false);
+                    event.consume();
                 }
+            });
 
-                event.consume();
-            }
-        });
+        root.getChildren().add((target));
+        }
 
-        root.getChildren().add(source);
-        root.getChildren().add(target);
-        root.getChildren().add((target3));
+
+
         stage.setScene(scene);
         stage.show();
     }
