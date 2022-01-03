513ddbd7a48f2b3aaae6d407b0988677048cff8e
Cyberooo <julien.erhard@edu.univ-fcomte.fr>
2022-01-03 23:22:35 +0100

    truc

--- src/newton/controller/ControllerDragAndDrop.java
+++ src/newton/controller/ControllerDragAndDrop.java
@@ -5,26 +5,50 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.*;
 
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.Objects;
 
 
 public class ControllerDragAndDrop extends Controller {
+    ImageView imageDuBled;
     @FXML ImageView pion;
+    @FXML Label compteurJaune;
+    @FXML Label compteurBleu;
+    @FXML Label compteurVert;
+    @FXML Label compteurRouge;
+    public int compteurJ;
+    public int compteurB;
+    public int compteurV;
+    public int compteurR;
+
+    @FXML
+    public void initialize() {
+        compteurJaune.setText("Jaune: 0");
+        compteurBleu.setText("Bleu: 0");
+        compteurVert.setText("Vert: 0");
+        compteurRouge.setText("Rouge: 0");
+        compteurJ = 0;
+        compteurB = 0;
+        compteurV = 0;
+        compteurR = 0;
+    }
 
     @FXML
     public void dragDetected(MouseEvent event) {
         ImageView source = (ImageView) event.getSource();
-        if (source.getId() != null && source.getId().equals("pion")) {
-            Dragboard db = source.startDragAndDrop(TransferMode.ANY);
+        if (source.getId() != null && source.getId().contains("pion")) {
+            Dragboard db = source.startDragAndDrop(TransferMode.MOVE);
 
             ClipboardContent content = new ClipboardContent();
             content.putImage(source.getImage());
             db.setContent(content);
+            imageDuBled = source;
         }
 
         event.consume();
@@ -45,24 +69,55 @@ public class ControllerDragAndDrop extends Controller {
     public void dragOver(DragEvent event) {
         ImageView source = (ImageView) event.getSource();
 
-        if (event.getGestureSource() != source && event.getDragboard().hasImage()) {
-            event.acceptTransferModes(TransferMode.MOVE);
-        }
+        if (source.getId() == null || source.getId().contains("pomme") || source.getId().contains("potion")) {
+            if (event.getGestureSource() != source && event.getDragboard().hasImage()) {
+                event.acceptTransferModes(TransferMode.MOVE);
+            }
 
-        event.consume();
+            event.consume();
+        }
     }
 
     public void dragDropped(DragEvent event) {
-        ImageView target = (ImageView) event.getSource();
-        Dragboard db = event.getDragboard();
-        boolean success = false;
-        if (db.hasImage()) {
-            target.setImage(db.getImage());
-            target.setId("pion");
-            success = true;
+        if (((ImageView) event.getSource()).getId() != null && ((ImageView) event.getSource()).getId().contains("pomme")) {
+            if ("pion".equals(imageDuBled.getId())) {
+                compteurR += 1;
+                compteurRouge.setText("Rouge: " + compteurR);
+            } else if ("pion2".equals(imageDuBled.getId())) {
+                compteurV += 1;
+                compteurVert.setText("Vert: " + compteurV);
+            } else if ("pion3".equals(imageDuBled.getId())) {
+                compteurB += 1;
+                compteurBleu.setText("Bleu: " + compteurB);
+            } else if ("pion4".equals(imageDuBled.getId())) {
+                compteurJ += 1;
+                compteurJaune.setText("Jaune: " + compteurJ);
+            }
+        } else if (((ImageView) event.getSource()).getId().contains("potion")) {
+            if ("pion".equals(imageDuBled.getId())) {
+                compteurR += 2;
+                compteurRouge.setText("Rouge: " + compteurR);
+            } else if ("pion2".equals(imageDuBled.getId())) {
+                compteurV += 2;
+                compteurVert.setText("Vert: " + compteurV);
+            } else if ("pion3".equals(imageDuBled.getId())) {
+                compteurB += 2;
+                compteurBleu.setText("Bleu: " + compteurB);
+            } else if ("pion4".equals(imageDuBled.getId())) {
+                compteurJ += 2;
+                compteurJaune.setText("Jaune: " + compteurJ);
+            }
         }
-        event.setDropCompleted(success);
+        ImageView target = (ImageView) event.getSource();
+            Dragboard db = event.getDragboard();
+            boolean success = false;
+            if (db.hasImage()) {
+                target.setImage(imageDuBled.getImage());
+                target.setId(imageDuBled.getId());
+                success = true;
+            }
+            event.setDropCompleted(success);
 
-        event.consume();
+            event.consume();
     }
 }
--- src/newton/controller/ControllerPlateauEurope.java
+++ src/newton/controller/ControllerPlateauEurope.java
@@ -1,7 +1,14 @@
 package newton.controller;
 
+import javafx.fxml.FXML;
+
 public class ControllerPlateauEurope extends Controller{
     public ControllerPlateauEurope() {
         super();
+    }
+
+    @FXML
+    public void initialize() {
+
     }
 }
