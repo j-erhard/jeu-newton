d33a89c704f510e4102dc17c7e5981a44cd3d9ac
ImSlaym <72543891+ImSlaym@users.noreply.github.com>
2022-01-12 00:04:18 +0100

    hashmap + arbre

--- src/client/controller/ControllerDragAndDrop.java
+++ src/client/controller/ControllerDragAndDrop.java
@@ -7,6 +7,9 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Random;
 
 
@@ -15,7 +18,6 @@ public class ControllerDragAndDrop extends Controller {
     ImageView pionDragged;
     String targetTempId1 = "start";
     String targetTempId2;
-    @FXML ImageView pion;
     @FXML Label compteurJaune;
     @FXML Label compteurBleu;
     @FXML Label compteurVert;
@@ -24,9 +26,82 @@ public class ControllerDragAndDrop extends Controller {
     public int compteurB;
     public int compteurV;
     public int compteurR;
+    /*
+    @FXML ImageView case1;
+    @FXML ImageView case2;
+    @FXML ImageView case3;
+    @FXML ImageView case4;
+    @FXML ImageView case5;
+    @FXML ImageView case6;
+    @FXML ImageView case7;
+    @FXML ImageView case8;
+    @FXML ImageView case9;
+    @FXML ImageView case10;
+    @FXML ImageView case11;
+    @FXML ImageView case12;
+    @FXML ImageView case13;
+    @FXML ImageView case14;
+    @FXML ImageView case15;
+    @FXML ImageView case16;
+    @FXML ImageView case17;
+    @FXML ImageView case18;
+    @FXML ImageView case19;
+    @FXML ImageView case20;
+    @FXML ImageView case21;
+    @FXML ImageView case22;
+    @FXML ImageView case23;
+    @FXML ImageView case24;
+    @FXML ImageView case25;
+    @FXML ImageView case26;
+    @FXML ImageView pomme1;
+    @FXML ImageView pomme2;
+    @FXML ImageView pomme3;
+    @FXML ImageView potion1;
+    @FXML ImageView potion2;
+    @FXML ImageView potion3;
+    @FXML ImageView potion4;
+     */
+    HashMap<String, ArrayList<String>> hashMap = new HashMap<>();
+
+    public void createTree() {
+        hashMap.put("start", new ArrayList<>(Arrays.asList("case1", "case6", "case19")));
+        hashMap.put("case1", new ArrayList<>(Arrays.asList("start", "case2", "case3")));
+        hashMap.put("case2", new ArrayList<>(Arrays.asList("case1")));
+        hashMap.put("case3", new ArrayList<>(Arrays.asList("case1", "pomme1")));
+        hashMap.put("pomme1", new ArrayList<>(Arrays.asList("case3", "case4")));
+        hashMap.put("case4", new ArrayList<>(Arrays.asList("pomme1", "case5")));
+        hashMap.put("case5", new ArrayList<>(Arrays.asList("case4", "potion1")));
+        hashMap.put("potion1", new ArrayList<>(Arrays.asList("case5")));
+        hashMap.put("case6", new ArrayList<>(Arrays.asList("start", "case7")));
+        hashMap.put("case7", new ArrayList<>(Arrays.asList("case6", "pomme2")));
+        hashMap.put("pomme2", new ArrayList<>(Arrays.asList("case7", "case8", "case12")));
+        hashMap.put("case8", new ArrayList<>(Arrays.asList("pomme2", "case9")));
+        hashMap.put("case9", new ArrayList<>(Arrays.asList("case8", "case10")));
+        hashMap.put("case10", new ArrayList<>(Arrays.asList("case9", "case11")));
+        hashMap.put("case11", new ArrayList<>(Arrays.asList("case10", "potion2")));
+        hashMap.put("potion2", new ArrayList<>(Arrays.asList("case11", "case16")));
+        hashMap.put("case12", new ArrayList<>(Arrays.asList("pomme2", "case13", "case17")));
+        hashMap.put("case13", new ArrayList<>(Arrays.asList("case12", "case14")));
+        hashMap.put("case14", new ArrayList<>(Arrays.asList("case13", "case15")));
+        hashMap.put("case15", new ArrayList<>(Arrays.asList("case14", "case16")));
+        hashMap.put("case16", new ArrayList<>(Arrays.asList("case15", "potion2")));
+        hashMap.put("case17", new ArrayList<>(Arrays.asList("case12", "case18")));
+        hashMap.put("case18", new ArrayList<>(Arrays.asList("case17", "potion3")));
+        hashMap.put("potion3", new ArrayList<>(Arrays.asList("case18")));
+        hashMap.put("case19", new ArrayList<>(Arrays.asList("start", "case20")));
+        hashMap.put("case20", new ArrayList<>(Arrays.asList("case19", "case21", "case24")));
+        hashMap.put("case21", new ArrayList<>(Arrays.asList("case20", "pomme3")));
+        hashMap.put("pomme3", new ArrayList<>(Arrays.asList("case21", "case22")));
+        hashMap.put("case22", new ArrayList<>(Arrays.asList("pomme3", "case23")));
+        hashMap.put("case23", new ArrayList<>(Arrays.asList("case22")));
+        hashMap.put("case24", new ArrayList<>(Arrays.asList("case20", "case25")));
+        hashMap.put("case25", new ArrayList<>(Arrays.asList("case24", "case26")));
+        hashMap.put("case26", new ArrayList<>(Arrays.asList("case25")));
+    }
 
     @FXML
     public void initialize() {
+        createTree();
         compteurJaune.setText("Jaune: 0");
         compteurBleu.setText("Bleu: 0");
         compteurVert.setText("Vert: 0");
@@ -68,10 +143,11 @@ public class ControllerDragAndDrop extends Controller {
 
     @FXML
     public void dragOver(DragEvent event) {
-        ImageView source = (ImageView) event.getSource();
-
-        if (source.getId() == null || source.getId().contains("pomme") || source.getId().contains("potion")) {
-            if (event.getGestureSource() != source && event.getDragboard().hasImage()) {
+        ImageView target = (ImageView) event.getSource();
+        String targetId = target.getId();
+        ArrayList<String> possibleTarget = hashMap.get(targetTempId1);
+        if (possibleTarget.contains(targetId)) {
+            if (event.getGestureSource() != target && event.getDragboard().hasImage()) {
                 event.acceptTransferModes(TransferMode.MOVE);
             }
 
@@ -80,30 +156,31 @@ public class ControllerDragAndDrop extends Controller {
     }
 
     public void dragDropped(DragEvent event) {
-        if (((ImageView) event.getSource()).getId() != null && ((ImageView) event.getSource()).getId().contains("pomme")) {
+        String url = ((ImageView) event.getSource()).getImage().getUrl();
+        if (((ImageView) event.getSource()).getId() != null && url.contains("pomme")) {
             if ("pion".equals(pionDragged.getId())) {
-                compteurR += 1;
-                compteurRouge.setText("Rouge: " + compteurR);
+                compteurB += 1;
+                compteurBleu.setText("Bleu: " + compteurB);
             } else if ("pion2".equals(pionDragged.getId())) {
                 compteurV += 1;
                 compteurVert.setText("Vert: " + compteurV);
             } else if ("pion3".equals(pionDragged.getId())) {
-                compteurB += 1;
-                compteurBleu.setText("Bleu: " + compteurB);
+                compteurR += 1;
+                compteurRouge.setText("Rouge: " + compteurR);
             } else if ("pion4".equals(pionDragged.getId())) {
                 compteurJ += 1;
                 compteurJaune.setText("Jaune: " + compteurJ);
             }
-        } else if (((ImageView) event.getSource()).getId() != null && ((ImageView) event.getSource()).getId().contains("potion")) {
+        } else if (((ImageView) event.getSource()).getId() != null && url.contains("potion")) {
             if ("pion".equals(pionDragged.getId())) {
-                compteurR += 2;
-                compteurRouge.setText("Rouge: " + compteurR);
+                compteurB += 2;
+                compteurBleu.setText("Bleu: " + compteurB);
             } else if ("pion2".equals(pionDragged.getId())) {
                 compteurV += 2;
                 compteurVert.setText("Vert: " + compteurV);
             } else if ("pion3".equals(pionDragged.getId())) {
-                compteurB += 2;
-                compteurBleu.setText("Bleu: " + compteurB);
+                compteurR += 2;
+                compteurRouge.setText("Rouge: " + compteurR);
             } else if ("pion4".equals(pionDragged.getId())) {
                 compteurJ += 2;
                 compteurJaune.setText("Jaune: " + compteurJ);
